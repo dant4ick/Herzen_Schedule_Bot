7bdc741f0e72315327aef4b57c72e8f968764bfb
vrnnncl <97320132+vrnnncl@users.noreply.github.com>
2022-08-30 23:33:58 +0300

    Fix: logging
    BasicConfig has no 'encoding' argument at python 3.8

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import database
 from config import TELEGRAM_TOKEN
 
-logging.basicConfig(filename=datetime.now().strftime('logs/%d-%m-%Y_%H-%M.log'), encoding='UTF-8', level=logging.INFO)
+logging.basicConfig(filename=datetime.now().strftime('logs/%d-%m-%Y_%H-%M.log'), level=logging.INFO)
 
 storage = MemoryStorage()
 bot = Bot(token=TELEGRAM_TOKEN, parse_mode='HTML')
