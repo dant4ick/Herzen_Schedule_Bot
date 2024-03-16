94cbc44808c4123495d3751318c6c48dd071a432
dant4ick <97320132+dant4ick@users.noreply.github.com>
2024-03-16 13:45:21 +0300

    modify: start_example.sh
    enable --debug mode by default

--- start_example.sh
+++ start_example.sh
@@ -9,6 +9,6 @@ export WEBAPP_PORT="5000"
 export DONATE_URL="https://pay.cloudtips.ru/p/0a19cb8e"
 export SUBSCRIBE_URL="https://boosty.to/dant4ick"
 
-until python3 run.py; do
+until python3 run.py --debug; do
     echo "The program crashed at `date +%H:%M:%S`. Restarting the script..."
 done
